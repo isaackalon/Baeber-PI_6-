119fc1581480d81234ef16eb342fe774d4ca7444
Isaac Luiz <91209472+isaackalon@users.noreply.github.com>
2026-09-21 21:48:39 -0300

    Preenchimento da tabela clientes.
    Primeiro preenchimento da tabela clientes.

---
+++ SCRIPT_1.1.sql
@@ -0,0 +1,23 @@
+select * from agendamentos;
+select * from clientes c 
+
+INSERT INTO clientes  (nome, telefone, email, data_cadastro)
+VALUES ('Tais', '12988179932', 'tais@gmail.com', '2000-01-01'),
+       ('Lelê', '12996505100', 'lele@gmail.com', '2017-01-01');
+
+INSERT INTO clientes  (nome, telefone, email, data_cadastro)
+VALUES ('Isa', '12996505100', 'isa@gmail.com', '2026-01-01'),
+       ('Maria', '12991636872', 'maria@gmail.com', '2025-01-01'),
+       ('Sandra', '12991918723', 'sandra@gmail.com', '1977-01-01'),
+       ('Daniel', '12991323637', 'daniel@gmail.com', '2003-01-01'),
+       ('Driele', '12991919191', 'driele@gmail.com', '1997-01-01'),  
+       ('Janaina', '12991793861', 'janaina@gmail.com', '1975-01-01'),
+       ('Maria Edna', '12991639873', 'mariaedna@gmail.com', '1971-01-01'),
+       ('Agnaldo', '12991431810', 'agnaldo@gmail.com', '1970-01-01');
+       
+       
+select * from entrada 
+
+INSERT INTO entrada (nome, email, data_criacao)
+VALUES ('bruna', 'buka@gmail.com', '2003-01-01'),
+       ('teodosio', 'teodosio@gmail.com', '2000-01-01');
